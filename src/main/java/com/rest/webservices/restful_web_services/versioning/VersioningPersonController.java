42eb8bf13951168d0554b7b41c9f1895d69c1eaa
safayilmaz56 <muhammedsafayilmaz56@gmail.com>
2024-09-02 13:27:24 +0300

    versiyonlama

--- src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api")
 public class VersioningPersonController {
 
+    // URI ile versiyonlama
     @GetMapping(path = "/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
         return new PersonV1("Safa Yılmaz");
@@ -18,6 +19,7 @@ public class VersioningPersonController {
         return new PersonV2("Safa" , "Yılmaz");
     }
 
+    // Request Parameter ile versiyonlama
     @GetMapping(path = "/person" , params = "version=1")
     public PersonV1 getFirstVersionOfPersonRequestParameter(){
         return new PersonV1("Safa Yılmaz");
@@ -25,6 +27,28 @@ public class VersioningPersonController {
 
     @GetMapping(path = "/person" , params = "version=2")
     public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2("Safa" , "Yılmaz");
+    }
+
+    // Request Header ile versiyonlama
+    @GetMapping(path = "/person/header" , headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("Safa Yılmaz");
+    }
+
+    @GetMapping(path = "/person/header" , headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader(){
+        return new PersonV2("Safa" , "Yılmaz");
+    }
+
+    // Media Type ile versiyonlama
+    @GetMapping(path = "/person/accept" , produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+        return new PersonV1("Safa Yılmaz");
+    }
+
+    @GetMapping(path = "/person/accept" , produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader(){
         return new PersonV2("Safa" , "Yılmaz");
     }
 }
