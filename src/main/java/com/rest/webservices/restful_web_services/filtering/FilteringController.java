2e98af21cf93777ec6cd0ff460403d3a1413112d
safayilmaz56 <muhammedsafayilmaz56@gmail.com>
2024-09-03 09:33:43 +0300

    implementing HATEOAS

---
+++ src/main/java/com/rest/webservices/restful_web_services/filtering/FilteringController.java
@@ -0,0 +1,16 @@
+package com.rest.webservices.restful_web_services.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+@RestController
+@RequestMapping("api")
+public class FilteringController {
+
+    @GetMapping("/v1/filtering")
+    public SomeBean filtering(){
+        return new SomeBean("value1","value2","value3");
+    }
+}
+*/
--- src/main/java/com/rest/webservices/restful_web_services/user/UserController.java
+++ src/main/java/com/rest/webservices/restful_web_services/user/UserController.java
@@ -1,11 +1,12 @@
 package com.rest.webservices.restful_web_services.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.management.AttributeNotFoundException;
 import java.net.URI;
 import java.util.List;
 
@@ -24,14 +25,20 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = userDaoService.findOne(id);
 
         if (user == null){
             throw new UserNotFoundException("id: " + id);
         }
 
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.
+                                 methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
     }
 
     @PostMapping("/users")
